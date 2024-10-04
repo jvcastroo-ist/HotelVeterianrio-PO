7db3c339ce2eb0fb81e42f188f74df759f824150
João Vitor Castro Silva <joao.castro.silva@tecnico.ulisboa.pt>
2024-10-04 16:36:42 +0100

    Add files via upload

---
+++ hva-skeleton/hva/core/Hotel.java
@@ -0,0 +1,27 @@
+package hva.core;
+
+import hva.core.exception.*;
+import java.io.*;
+import java.util.*;
+// FIXME import classes
+
+public class Hotel implements Serializable {
+
+  @Serial
+  private static final long serialVersionUID = 202407081733L;
+  
+  // FIXME define attributes
+  // FIXME define contructor(s)
+  // FIXME define more methods
+  
+  /**
+   * Read text input file and create corresponding domain entities.
+   * 
+   * @param filename name of the text input file
+   * @throws UnrecognizedEntryException if some entry is not correct
+   * @throws IOException if there is an IO erro while processing the text file
+   **/
+  void importFile(String filename) throws UnrecognizedEntryException, IOException /* FIXME maybe other exceptions */  {
+    //FIXME implement method
+  }
+}
