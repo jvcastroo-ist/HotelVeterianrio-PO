ac8d43b762fdb907f3a1cc629e0d69a21550bb7e
Vasco <vfreino@gmail.com>
2024-10-05 19:06:47 +0100

    toString

--- hva-skeleton/hva/core/Veterinario.java
+++ hva-skeleton/hva/core/Veterinario.java
@@ -2,16 +2,35 @@ package hva.core;
 import java.util.ArrayList;
 
 public class Veterinario extends Funcionario{
-    private ArrayList<Especie> _especies;
-    private Arraylist<RegistoVacina> _registoVacinas;
+  private ArrayList<Especie> _especies;
+  private Arraylist<RegistoVacina> _registoVacinas;
 
-    public Veterinario(String idVeterinario, String nome){
-        super(idVeterinario, nome);
-        _especies = new ArrayList<Especie>();
-        _registoVacinas = new ArrayList<RegistoVacina>();
+  public Veterinario(String idVeterinario, String nome){
+    super(idVeterinario, nome);
+    _especies = new ArrayList<Especie>();
+    _registoVacinas = new ArrayList<RegistoVacina>();
+  }
+
+
+  private ArrayList<String> stringResponsabilidade(){
+    ArrayList<String> idHabitats = new ArrayList<String>();
+
+    for(Especie h : _especies){
+      idHabitats.add(h.getIdEspecie());
     }
+    return idHabitats;
+  }
 
-    // Fazer derivado da classe abstrata
-    @Override
-    public String visualizaFuncionario();
+  // Fazer derivado da classe abstrata
+  @Override
+  public String visualiza(){
+    if(_especies.isEmpty()){
+      return String.format("VETERINÁRIO|%s|%s", getIdFuncionario(), getNome());
+    } else{
+      
+      String responsabilidades = String.join(",", stringResponsabilidade());
+
+      return String.format("VETERINÁRIO|%s|%s|%s", getIdFuncionario(), getNome(), responsabilidades);
+    }
+  }
 }
