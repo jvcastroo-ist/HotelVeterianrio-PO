deb1cfec451e8c80ee0365293e0c7b2e82943b3a
Joao Vitor <joao.castro.silva@tecnico.ulisboa.pt>
2024-10-11 21:58:24 +0100

    Mudanças no Animal e Habitat

--- hva-skeleton/hva/core/Animal.java
+++ hva-skeleton/hva/core/Animal.java
@@ -8,8 +8,8 @@ public class Animal implements Serializable {
   private final String _nome;
   private final Especie _especie;
   private Habitat _habitat;
-  private List<RegistoVacina> _registoVacinacao;
   private int _adequacao;
+  private List<RegistoVacina> _registoVacinacao;
             
   /**
   * Constructs a new Animal with the specified id, name, species, and habitat.
@@ -64,6 +64,15 @@ public class Animal implements Serializable {
   }
 
   /**
+   * Retrieves the habitat of the animal.
+   *
+   * @return the habitat of the animal.
+   */
+  public Habitat getHabitat() {
+    return _habitat;
+  }
+
+  /**
    * Retrieves the vaccination record of the animal.
    *
    * @return A string representation of the vaccination record. If the record is empty, returns "VOID".
@@ -78,13 +87,50 @@ public class Animal implements Serializable {
       return String.join(",", danos);
   }
 
+  public long satisfacao() {
+    // Numero de especies diferentes no habitat
+    int especieDiferente = _habitat.getAnimals().size() - (especieIgual() + 1);
+    // AREA/POPULACAO
+    double espacoMedio = _habitat.getArea()/_habitat.getAnimals().size();
+    // satisfacao arredondada ao inteiro mais proximo
+    return Math.round(20 + 3*especieIgual() - 2*especieDiferente + espacoMedio + _adequacao);
+  }
+
   /**
-   * Retrieves the habitat of the animal.
+   * Calculates the number of animals of the same species in the habitat.
    *
-   * @return the habitat of the animal.
+   * @return the number of animals of the same species minus one(itself).
    */
-  public Habitat getHabitat() {
-    return _habitat;
+  public int especieIgual() {
+    return _habitat.getAnimals(_especie).size() - 1;
+  }
+
+  /**
+   * Sets the adequacy value for the animal.
+   *
+   * @param valor the adequacy value to be set
+   */
+  public void setAdequacao(int valor) {
+    _adequacao = valor;
+  }
+
+  /**
+   * Adds a vaccination record to the animal's vaccination registry.
+   *
+   * @param rg the vaccination record to be added
+   */
+  public void addRegisto(RegistoVacina rg) {
+    _registoVacinacao.add(rg);
+  }
+
+  /**
+   * Transfers the animal to a new habitat.
+   *
+   * @param novoHabitat the new habitat to which the animal will be transferred
+   */
+  public void transfereAnimal(Habitat novoHabitat) {
+    _habitat = novoHabitat;
+    _adequacao = 0;
   }
 
   /**
